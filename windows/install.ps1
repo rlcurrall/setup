d9240ec33270f8168d6537a5fc2eef9f133e8fa7
Robb Currall <robb.currall@vantaca.com>
2024-08-09 16:01:32 -0400

    feat: add initial windows install and profile

---
+++ windows/install.ps1
@@ -0,0 +1,312 @@
+function Main() {
+    # ==========================================================================
+    # CLI Tools
+    #
+    # This section installs command-line tools that are commonly used.
+    # ==========================================================================
+    #region cli tools
+    printSectionMessage("Installing CLI Tools")
+
+    # see: https://git-scm.com/doc
+    winget install Git.MinGit.BusyBox
+
+    # see: https://github.com/coreybutler/nvm-windows
+    winget install CoreyButler.NVMforWindows
+
+    # see: https://learn.microsoft.com/en-us/cli/azure
+    winget install Microsoft.AzureCLI
+
+    # see: https://docs.stripe.com/stripe-cli
+    winget install Stripe.StripeCLI
+
+    # see: https://www.terraform.io
+    winget install Hashicorp.Terraform
+
+    # see: https://jqlang.github.io/jq
+    winget install jqlang.jq
+
+    # see: https://github.com/eza-community/eza -- use older version to fix bug
+    winget install eza-community.eza -v 0.18.21
+
+    # see: https://github.com/BurntSushi/ripgrep
+    winget install BurntSushi.ripgrep.MSVC
+
+    # see: https://neovim.io
+    winget install Neovim.Neovim
+
+    # see: https://github.com/jftuga/less-Windows
+    winget install jftuga.less
+
+    # see: https://github.com/junegunn/fzf
+    winget install junegunn.fzf
+
+    # see: https://learn.microsoft.com/en-us/powershell/
+    winget install Microsoft.PowerShell
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+    #endregion
+
+    # ==========================================================================
+    # PowerShell 7
+    #
+    # This section installs the necessary PowerShell modules for integrating with
+    # Git and FZF.
+    # ==========================================================================
+    #region powershell 7
+    printSectionMessage("Installing PowerShell 7 Modules")
+
+    pwsh -c 'PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force'
+    pwsh -c 'Install-Module -Name PSFzf -Scope CurrentUser -Force'
+    #endregion
+
+    # ==========================================================================
+    # Languages
+    #
+    # This section installs programming languages that are commonly.
+    # ==========================================================================
+    #region languages
+    printSectionMessage("Installing Languages")
+
+    winget install Python.Python.3.12
+    winget install Microsoft.DotNet.SDK.8
+    winget install Microsoft.DotNet.SDK.6
+    winget install Microsoft.DotNet.Framework.DeveloperPack_4 -v 4.6.2
+
+    # todo: figure out how to install Node.js through nvm without restarting the shell
+    # nvm install 18
+    #endregion
+
+    # ==========================================================================
+    # Dev Tools
+    #
+    # This section installs development tools that are commonly used.
+    # ==========================================================================
+    #region dev tools
+    printSectionMessage("Installing Development Tools")
+
+    winget install Microsoft.VisualStudioCode
+    winget install Microsoft.VisualStudio.2022.Professional
+    winget install Microsoft.VisualStudio.2022.BuildTools
+    winget install Docker.DockerDesktop
+    winget install Microsoft.Azure.StorageExplorer
+    winget install Postman.Postman
+
+    do {
+        if (!($Alacritty = Read-Host "`nWould you like to install the Alacritty terminal? (y/N)")) {
+            $Alacritty = "n"
+        }
+    } while ($Alacritty -notin @("y", "n"))
+
+    if ($Alacritty -eq "y") {
+        # see: https://alacritty.org/config-alacritty.html
+        winget install Alacritty.Alacritty
+    }
+
+    do {
+        if (!($Rider = Read-Host "`nWould you like to install Rider? (y/N)")) {
+            $Rider = "n"
+        }
+    } while ($Rider -notin @("y", "n"))
+
+    if ($Rider -eq "y") {
+        winget install JetBrains.Rider
+    }
+
+    do {
+        if (!($SSMS = Read-Host "`nWould you like to install SQL Server Management Studio? (y/N)")) {
+            $SSMS = "n"
+        }
+    } while ($SSMS -notin @("y", "n"))
+
+    if ($SSMS -eq "y") {
+        winget install Microsoft.SQLServerManagementStudio
+    }
+    #endregion
+
+    # ==========================================================================
+    # Apps
+    #
+    # This section installs applications that are commonly used by developers at
+    # Vantaca that are not development tools.
+    # ==========================================================================
+    #region apps
+    printSectionMessage("Installing Applications")
+
+    winget install 9NP355QT2SQB # Azure VPN Client
+    winget install Microsoft.Teams
+    winget install Microsoft.PowerToys
+    #endregion
+
+    # ==========================================================================
+    # Lazy TUI Tools
+    #
+    # This section installs lazy TUI tools that might be useful for command-line
+    # users.
+    # ==========================================================================
+    #region lazy tui tools
+    do {
+        if (!($LazyTui = Read-Host "`nWould you like to install lazy TUIs? (y/N)")) {
+            $LazyTui = "n"
+        }
+    } while ($LazyTui -notin @("y", "n"))
+
+    if ($LazyTui -eq "y") {
+        winget install JesseDuffield.lazygit
+        winget install JesseDuffield.Lazydocker
+    }
+    #endregion
+
+    # ==========================================================================
+    # Browsers
+    #
+    # This section allows the user to install browsers they would like to use.
+    # ==========================================================================
+    #region browsers
+    printSectionMessage("Installing Browsers")
+
+    do {
+        if (!($Vivaldi = Read-Host "`nWould you like to install Vivaldi? (y/N)")) {
+            $Vivaldi = "n"
+        }
+    } while ($Vivaldi -notin @("y", "n"))
+
+    if ($Vivaldi -eq "y") {
+        winget install Vivaldi.Vivaldi
+    }
+
+    do {
+        if (!($Chrome = Read-Host "`nWould you like to install Google Chrome? (y/N)")) {
+            $Chrome = "n"
+        }
+    } while ($Chrome -notin @("y", "n"))
+
+    if ($Chrome -eq "y") {
+        winget install Google.Chrome
+    }
+
+    do {
+        if (!($Firefox = Read-Host "`nWould you like to install Mozilla Firefox? (y/N)")) {
+            $Firefox = "n"
+        }
+    } while ($Firefox -notin @("y", "n"))
+
+    if ($Firefox -eq "y") {
+        winget install Mozilla.Firefox
+    }
+    #endregion
+
+    # ==========================================================================
+    # Password Managers
+    #
+    # This section allows the user to install 1Password if they would like to use
+    # it as their password manager.
+    #
+    # NOTE: LastPass does not work through winget, so it is not included here.
+    # ==========================================================================
+    #region password managers
+    printSectionMessage("Installing Password Managers")
+
+    do {
+        if (!($1Password = Read-Host "`nWould you like to install 1Password? (y/N)")) {
+            $1Password = "n"
+        }
+    } while ($1Password -notin @("y", "n"))
+
+    if ($1Password -eq "y") {
+        winget install AgileBits.1Password
+        winget install AgileBits.1Password.CLI
+    }
+    #endregion
+
+    # ==========================================================================
+    # Screen Recording Tools
+    #
+    # This section allows the user to install screen recording tools if they would
+    # like to use them.
+    # ==========================================================================
+    #region screen recording tools
+    printSectionMessage("Installing Screen Recording Tools")
+
+    do {
+        if (!($Recording = Read-Host "`nWould you like to install recording tools? (y/N)")) {
+            $Recording = "n"
+        }
+    } while ($Recording -notin @("y", "n"))
+
+    if ($Recording -eq "y") {
+        winget install OBSProject.OBSStudio
+        winget install VideoLAN.VLC
+        winget install Gyan.FFmpeg
+    }
+    #endregion
+
+    # ==========================================================================
+    # Configure Tools
+    #
+    # This section configures the tools that were installed in the previous steps.
+    # This includes setting up Git, PowerShell, and other tools.
+    # ==========================================================================
+    #region configure tools
+    printSectionMessage("Configuring Tools")
+
+    git config --global core.editor "nvim"
+    git config --global core.pager "less"
+
+    $GitName = Read-Host "What is your name for git?"
+    git config --global user.name $GitName
+
+    $GitEmail = Read-Host "What is your email for git?"
+    git config --global user.email $GitEmail
+
+    # Set sslcainfo configuration so cloning over HTTPS resolves correctly
+    git config --global http.sslcainfo "$env:localappdata\Microsoft\WinGet\Packages\Git.MinGit.BusyBox_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\etc\ssl\certs\ca-bundle.crt"
+
+    do {
+        if (!($UseHomeDir = Read-Host "`nWould you like to use your home directory for your PowerShell profile? (y/N)")) {
+            $UseHomeDir = "n"
+        }
+    } while ($UseHomeDir -notin @("y", "n"))
+
+    if ($UseHomeDir -eq "y") {
+        Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Personal" -Value "$HOME\Documents"
+    }
+    #endregion
+
+    # ==========================================================================
+    # Setup NeoVim
+    #
+    # This section sets up NeoVim with the necessary configuration if the user
+    # chooses to use it as their editor.
+    # ==========================================================================
+    #region setup neovim
+    # printSectionMessage("Setting Up NeoVim")
+
+    # todo: this is not working yet, requires manual installation of the C++ build
+    # tools through Visual Studio Installer. Need to figure out how to automate.
+
+    # do {
+    #     if (!($SetupNvim = Read-Host "Would you like to setup NeoVim? (y/N)")) {
+    #         $SetupNvim = "n"
+    #     }
+    # } while ($SetupNvim -notin @("y", "n"))
+
+    # if ($SetupNvim -eq "y") {
+    #     Read-Host "Installing dependencies for NeoVim.`nBe sure select the option to add LLVM to your PATH when prompted.`n`nPress Enter to continue."
+    #     winget install llvm.llvm -i
+    #     winget install Kitware.CMake
+    #     winget install ezwinports.make
+
+    #     # Reload path to get access to new tools
+    #     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+    #     git clone https://github.com/nvim-lua/kickstart.nvim.git $env:LOCALAPPDATA\nvim\
+    # }
+    #endregion
+}
+
+function printSectionMessage($message) {
+    $Separator = "".PadLeft(80, "=")
+    Write-Host "`n${Separator}`n${message}`n${Separator}`n" -ForegroundColor DarkYellow
+}
+
+Main
---
+++ windows/profile.ps1
@@ -0,0 +1,113 @@
+Import-Module PSReadLine
+
+# Ensure PATH variable is loaded with both machine and user defined path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+# Searching for commands with up/down arrow is really handy.  The
+# option "moves to end" is useful if you want the cursor at the end
+# of the line while cycling through history like it does w/o searching,
+# without that option, the cursor will remain at the position it was
+# when you used up arrow, which can be useful if you forget the exact
+# string you started the search on.
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Tabbing to be like zsh rather than bash
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Jump words
+Set-PSReadlineKeyHandler -Key Ctrl+LeftArrow BackwardWord
+Set-PSReadlineKeyHandler -Key Ctrl+RightArrow ForwardWord
+
+# Wrap demanding commands in an idle event to prevent blocking the prompt
+$null = Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount 1 -Action {
+    # Configure fzf for reverse history search
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+}
+
+#region prompt
+# Copy of the robbyrussell theme from Oh My Posh (https://ohmyposh.dev)
+function Prompt {
+    $ArrowIcon = [System.Convert]::toInt32("279C",16)
+    $ArrowIcon = [System.Char]::ConvertFromUtf32($ArrowIcon)
+    $XmarkIcon = [System.Convert]::toInt32("2717",16)
+    $XmarkIcon = [System.Char]::ConvertFromUtf32($XmarkIcon)
+
+    if ($?) {
+        Write-Host $ArrowIcon -NoNewline -ForegroundColor Green
+    } else {
+        Write-Host $ArrowIcon -NoNewline -ForegroundColor Red
+    }
+
+    if ($(Split-Path -Path $pwd -Leaf) -eq $(Split-Path -Path $Home -Leaf)) {
+        Write-Host ("  ~") -NoNewline -ForegroundColor Cyan
+    } else {
+        Write-Host ("  " + $(Split-Path -Path $pwd -Leaf)) -NoNewline -ForegroundColor Cyan
+    }
+
+    $gitStatus = Get-GitStatus
+    if ($gitStatus) {
+        Write-Host " git:(" -NoNewline -ForegroundColor Blue
+        Write-Host "$($gitStatus.Branch)" -NoNewline -ForegroundColor Red
+        Write-Host ")" -NoNewline -ForegroundColor Blue
+
+        if ($gitStatus.Working.Length -gt 0) {
+            Write-Host " $XmarkIcon" -NoNewline -ForegroundColor Yellow
+        }
+    }
+
+    return " "
+}
+#endregion
+
+#region directory movement aliases
+function __up_one_dir { Set-Location ..; }
+function __up_two_dir { Set-Location ../..; }
+function __up_three_dir { Set-Location ../../..; }
+function __up_four_dir { Set-Location ../../../..; }
+
+Set-Alias -Name '..' __up_one_dir
+Set-Alias -Name '...' __up_two_dir
+Set-Alias -Name '....' __up_three_dir
+Set-Alias -Name '.....' __up_four_dir
+#endregion
+
+#region eza aliases
+function __eza_l { eza -lha; }
+function __eza_la { eza -alH; }
+function __eza_ll { eza -lH --git --hyperlink --group-directories-first; }
+
+Set-Alias -Name 'ls' eza
+Set-Alias -Name 'l' __eza_l
+Set-Alias -Name 'la' __eza_la
+Set-Alias -Name 'll' __eza_ll
+#endregion
+
+#region git aliases
+function __git_add { git add $args; }
+function __git_status { git status $args; }
+function __git_log { git log  --abbrev-commit --pretty="%C(red bold)%h%Creset | %C(yellow bold)%d%Creset %s %Cgreen(%cr)%Creset [%an]"; }
+function __git_log_graph { git log --graph  --abbrev-commit --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'; }
+
+Set-Alias -Name 'ga' __git_add
+Set-Alias -Name 'gst' __git_status
+Set-Alias -Name 'gll' __git_log
+Set-Alias -Name 'glg' __git_log_graph
+#endregion
+
+#region bash-like aliases
+function __which($name) {
+    Try {
+        $info = Get-Command -ErrorAction Stop $name
+        $info.Definition
+    } Catch {
+        Write-Host "which: $name not found" -ForegroundColor Red
+    }
+}
+
+Set-Alias -Name 'which' __which
+Set-Alias -Name 'touch' New-Item
+#endregion
+
+Set-Alias -Name 'tf' terraform
