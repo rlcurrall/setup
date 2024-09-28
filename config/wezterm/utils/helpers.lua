e93dac005bed36475c798349096c7c6ddf4a1d30
Robb Currall <robb.currall@vantaca.com>
2024-09-28 14:55:13 -0400

    feat: configure wezterm unix domain

--- config/wezterm/utils/helpers.lua
+++ config/wezterm/utils/helpers.lua
@@ -1,14 +1,14 @@
-local M = {}
 local wezterm = require("wezterm")
+local module = {}
 
-M.get_appearance = function()
+module.get_appearance = function()
 	if wezterm.gui then
 		return wezterm.gui.get_appearance()
 	end
 	return "Dark"
 end
 
-M.scheme_for_appearance = function(appearance)
+module.scheme_for_appearance = function(appearance)
 	if appearance:find("Dark") then
 		return "Tokyo Night"
 	else
@@ -16,19 +16,19 @@ M.scheme_for_appearance = function(appearance)
 	end
 end
 
-M.is_dark = M.get_appearance():find("Dark")
-M.theme = M.scheme_for_appearance(M.get_appearance())
+module.is_dark = module.get_appearance():find("Dark")
+module.theme = module.scheme_for_appearance(module.get_appearance())
 
-M.is_linux = function()
+module.is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
 end
 
-M.is_darwin = function()
+module.is_darwin = function()
 	return wezterm.target_triple:find("darwin") ~= nil
 end
 
-M.is_windows = function()
+module.is_windows = function()
 	return wezterm.target_triple:find("windows") ~= nil
 end
 
-return M
+return module
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,27 +1,35 @@
 local wezterm = require("wezterm")
-local h = require("utils.helpers")
+local helpers = require("utils.helpers")
 
 local config = wezterm.config_builder()
 
 config.window_close_confirmation = "NeverPrompt"
 
-config.color_scheme = h.theme
+config.color_scheme = helpers.theme
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
-config.show_new_tab_button_in_tab_bar  = false
+config.show_new_tab_button_in_tab_bar = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.window_frame = {
-	font = wezterm.font("Fira Code", {}),
+	font = wezterm.font("Fira Code"),
 }
 
 config.font_size = 14
-config.font = wezterm.font("Fira Code", {})
+config.font = wezterm.font("Fira Code")
 
 config.enable_wayland = true
 
-if h.is_windows() then
-	config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-NoLogo' }
+if helpers.is_windows() then
+	config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-NoLogo" }
 end
+
+config.unix_domains = { { name = "unix" } }
+
+config.keys = {
+	{ key = "Space", mods = "CTRL", action = wezterm.action.ActivateCopyMode },
+	{ key = "a", mods = "ALT", action = wezterm.action.AttachDomain("unix") },
+	{ key = "d", mods = "ALT", action = wezterm.action.DetachDomain({ DomainName = "unix" }) },
+}
 
 return config
