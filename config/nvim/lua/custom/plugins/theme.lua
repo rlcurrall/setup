53b07992270cb69507c9422b2dda27d7df2cbaaf
Robb Currall <rlcurrall@gmail.com>
2025-08-18 23:01:21 -0400

    chore: switch to rose pine theme

--- config/nvim/lua/custom/plugins/theme.lua
+++ config/nvim/lua/custom/plugins/theme.lua
@@ -1,44 +1,51 @@
 return {
+  -- -- Tokyo Night theme
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-storm'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  --
+  -- {
+  --   'f-person/auto-dark-mode.nvim',
+  --   opts = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.cmd.colorscheme 'tokyonight-storm'
+  --     end,
+  --     set_light_mode = function()
+  --       vim.cmd.colorscheme 'tokyonight-day'
+  --     end,
+  --   },
+  -- },
+
+  -- -- Rose Pine theme
   {
-    'folke/tokyonight.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false,
     priority = 1000,
     opts = {},
     init = function()
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'rose-pine-moon'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     disable_background = true,
-  --     styles = {
-  --       transparency = true,
-  --     },
-  --   },
-  --   init = function()
-  --     vim.cmd.colorscheme 'rose-pine-moon'
-  --     vim.cmd.hi 'Comment gui=none'
-  --
-  --     -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  --     -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-  --   end,
-  -- },
 
   {
     'f-person/auto-dark-mode.nvim',
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.cmd.colorscheme 'tokyonight-storm'
+        vim.cmd.colorscheme 'rose-pine-moon'
       end,
       set_light_mode = function()
-        vim.cmd.colorscheme 'tokyonight-day'
+        vim.cmd.colorscheme 'rose-pine-dawn'
       end,
     },
   },
