68beca6d2ac8887bdea6020f76b5b018994f0e1e
Robb Currall <rlcurrall@gmail.com>
2025-08-18 23:08:28 -0400

    chore: switch to catppuccin theme

--- config/nvim/lua/custom/plugins/theme.lua
+++ config/nvim/lua/custom/plugins/theme.lua
@@ -25,14 +25,40 @@ return {
   -- },
 
   -- -- Rose Pine theme
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   init = function()
+  --     vim.cmd.colorscheme 'rose-pine-moon'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  --
+  -- {
+  --   'f-person/auto-dark-mode.nvim',
+  --   opts = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.cmd.colorscheme 'rose-pine-moon'
+  --     end,
+  --     set_light_mode = function()
+  --       vim.cmd.colorscheme 'rose-pine-dawn'
+  --     end,
+  --   },
+  -- },
+
+  -- Catppuccin theme
   {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     lazy = false,
     priority = 1000,
     opts = {},
     init = function()
-      vim.cmd.colorscheme 'rose-pine-moon'
+      vim.cmd.colorscheme 'catppuccin-frappe'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -42,11 +68,12 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.cmd.colorscheme 'rose-pine-moon'
+        vim.cmd.colorscheme 'catppuccin-frappe'
       end,
       set_light_mode = function()
-        vim.cmd.colorscheme 'rose-pine-dawn'
+        vim.cmd.colorscheme 'catppuccin-latte'
       end,
     },
   },
+  }
 }
