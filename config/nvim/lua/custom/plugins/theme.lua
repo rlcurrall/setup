1e3739e3f09ea4939a489d195fec9c46b30417fd
Robb Currall <rlcurrall@gmail.com>
2026-06-15 19:52:31 -0400

    chore(nvim): switch to rosepine

--- config/nvim/lua/custom/plugins/theme.lua
+++ config/nvim/lua/custom/plugins/theme.lua
@@ -24,41 +24,15 @@ return {
   --   },
   -- },
 
-  -- -- Rose Pine theme
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   init = function()
-  --     vim.cmd.colorscheme 'rose-pine-moon'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
-  --
-  -- {
-  --   'f-person/auto-dark-mode.nvim',
-  --   opts = {
-  --     update_interval = 1000,
-  --     set_dark_mode = function()
-  --       vim.cmd.colorscheme 'rose-pine-moon'
-  --     end,
-  --     set_light_mode = function()
-  --       vim.cmd.colorscheme 'rose-pine-dawn'
-  --     end,
-  --   },
-  -- },
-
-  -- Catppuccin theme
+  -- Rose Pine theme
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false,
     priority = 1000,
     opts = {},
     init = function()
-      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.colorscheme 'rose-pine-moon'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
@@ -68,11 +42,37 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.cmd.colorscheme 'catppuccin-frappe'
+        vim.cmd.colorscheme 'rose-pine-moon'
       end,
       set_light_mode = function()
-        vim.cmd.colorscheme 'catppuccin-latte'
+        vim.cmd.colorscheme 'rose-pine-dawn'
       end,
     },
   },
+
+  -- -- Catppuccin theme
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   init = function()
+  --     vim.cmd.colorscheme 'catppuccin-frappe'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  --
+  -- {
+  --   'f-person/auto-dark-mode.nvim',
+  --   opts = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.cmd.colorscheme 'catppuccin-frappe'
+  --     end,
+  --     set_light_mode = function()
+  --       vim.cmd.colorscheme 'catppuccin-latte'
+  --     end,
+  --   },
+  -- },
 }
