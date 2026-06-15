cbaa302fdcb25cc8c62559c7c22659ab822ed03a
Robb Currall <robb.currall@vantaca.com>
2026-06-15 19:42:05 -0400

    chore(nvim): remove redundant git signs setup

--- config/nvim/lua/custom/plugins/ui.lua
+++ config/nvim/lua/custom/plugins/ui.lua
@@ -1,18 +1,4 @@
 return {
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
   { -- Highlight todo, notes, etc in comments
     'folke/todo-comments.nvim',
     event = 'VimEnter',
